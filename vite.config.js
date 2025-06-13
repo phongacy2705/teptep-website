d1d0511863f7bc39105a8537d76b6ec8d41e0c6a
Phong Nguyen <142887546+phongacy2705@users.noreply.github.com>
2025-06-13 16:29:58 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Đảm bảo server dev chạy đúng cổng nếu cần
+  server: {
+    port: 5173, 
+  },
+  // Cấu hình cho quá trình build
+  build: {
+    outDir: 'dist' // Thư mục đầu ra sau khi build
+  }
 })
